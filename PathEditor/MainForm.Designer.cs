63847ab1c5d7b56155a48ddfd541a71ba725141b
Syntox32 <syntox32@gmail.com>
2015-08-30 11:38:38 +0200

    Initial commit

---
+++ PathEditor/MainForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PathEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnApply = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemBackup = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemAutoBackup = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemSwitch = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.menuItemInvalidate = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnApply
+            // 
+            this.btnApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnApply.Location = new System.Drawing.Point(311, 275);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(148, 23);
+            this.btnApply.TabIndex = 0;
+            this.btnApply.Text = "Apply Changes";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Location = new System.Drawing.Point(12, 59);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(447, 210);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolsToolStripMenuItem,
+            this.menuItemSwitch});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(471, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemBackup,
+            this.menuItemLoad,
+            this.menuItemInvalidate,
+            this.menuItemAutoBackup});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // menuItemBackup
+            // 
+            this.menuItemBackup.Name = "menuItemBackup";
+            this.menuItemBackup.Size = new System.Drawing.Size(211, 22);
+            this.menuItemBackup.Text = "Backup";
+            this.menuItemBackup.Click += new System.EventHandler(this.menuItemBackup_Click);
+            // 
+            // menuItemLoad
+            // 
+            this.menuItemLoad.Name = "menuItemLoad";
+            this.menuItemLoad.Size = new System.Drawing.Size(211, 22);
+            this.menuItemLoad.Text = "Load backup";
+            this.menuItemLoad.Click += new System.EventHandler(this.menuItemLoad_Click);
+            // 
+            // menuItemAutoBackup
+            // 
+            this.menuItemAutoBackup.Checked = true;
+            this.menuItemAutoBackup.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.menuItemAutoBackup.Name = "menuItemAutoBackup";
+            this.menuItemAutoBackup.Size = new System.Drawing.Size(211, 22);
+            this.menuItemAutoBackup.Text = "Auto backup before apply";
+            this.menuItemAutoBackup.Click += new System.EventHandler(this.menuItemAutoBackup_Click);
+            // 
+            // menuItemSwitch
+            // 
+            this.menuItemSwitch.Name = "menuItemSwitch";
+            this.menuItemSwitch.Size = new System.Drawing.Size(94, 20);
+            this.menuItemSwitch.Text = "Switch to User";
+            this.menuItemSwitch.Click += new System.EventHandler(this.menuItemSwitch_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 30);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAdd.Location = new System.Drawing.Point(93, 32);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(323, 20);
+            this.txtAdd.TabIndex = 6;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(422, 30);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(37, 23);
+            this.btnBrowse.TabIndex = 7;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // menuItemInvalidate
+            // 
+            this.menuItemInvalidate.Name = "menuItemInvalidate";
+            this.menuItemInvalidate.Size = new System.Drawing.Size(211, 22);
+            this.menuItemInvalidate.Text = "Invalidate paths";
+            this.menuItemInvalidate.Click += new System.EventHandler(this.menuItemInvalidate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 305);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Path Editor";
+            this.Load += new System.EventHandler(this.LoadMain);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.ToolStripMenuItem menuItemSwitch;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuItemBackup;
+        private System.Windows.Forms.ToolStripMenuItem menuItemAutoBackup;
+        private System.Windows.Forms.ToolStripMenuItem menuItemInvalidate;
+        private System.Windows.Forms.ToolStripMenuItem menuItemLoad;
+    }
+}
+
---
+++ PathEditor/MainForm.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime;
+using System.IO;
+
+namespace PathEditor
+{
+    public partial class MainForm : Form
+    {
+        private Context _currentContext { get; set; }
+        private bool _backupBeforeApply { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Controls
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            ApplyPrompt();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddNewVar(txtAdd.Text);
+        }
+
+        private void menuItemSwitch_Click(object sender, EventArgs e)
+        {
+            ToggleContext();
+        }
+
+        private void menuItemInvalidate_Click(object sender, EventArgs e)
+        {
+            InvalidatePaths(_currentContext);
+        }
+
+        private void menuItemAutoBackup_Click(object sender, EventArgs e)
+        {
+            menuItemAutoBackup.Checked = !menuItemAutoBackup.Checked;
+            _backupBeforeApply = menuItemAutoBackup.Checked;
+        }
+
+        private void menuItemBackup_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new SaveFileDialog())
+            {
+                dialog.DefaultExt = "txt";
+                dialog.FileName = GetBackupName();
+
+                dialog.ShowDialog();
+
+                CreateBackup(_currentContext, dialog.FileName);
+            }
+        }
+
+        private void menuItemLoad_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.CheckFileExists = true;
+                dialog.Multiselect = false;
+
+                dialog.ShowDialog();
+
+                LoadBackup(dialog.FileName);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                var result = dialog.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    if (Directory.Exists(dialog.SelectedPath))
+                    {
+                        txtAdd.BackColor = Color.LightGreen;
+                    }
+                    else
+                    {
+                        txtAdd.BackColor = Color.Red;
+                    }
+
+                    txtAdd.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        #endregion
+
+        private void LoadMain(object sender, EventArgs e)
+        {
+            _currentContext = Context.Global;
+            _backupBeforeApply = menuItemAutoBackup.Checked;
+
+            RefreshList();
+        }
+
+        private void ApplyPrompt()
+        {
+            var backup = _backupBeforeApply
+                ? "\nA backup of the existing path variable will be automatically created.\n"
+                : "\nIt is recommended to create a backup before making changes.\n";
+
+            DialogResult confirm = MessageBox.Show(
+                string.Format("You are about to apply new changes to the [{0}] PATH variable.\n{1}\nDo you wish to continue?",
+                    _currentContext.ToString(), backup),
+                "Do you wish to continue?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+
+            if (confirm == DialogResult.Yes)
+            {
+                var success = _backupBeforeApply 
+                    ? AutoBackup(_currentContext) 
+                    : true;
+
+                if (success)
+                {
+                    var pathVariable = ConstructPathFromList();
+                    SetPathVariable(_currentContext, pathVariable);
+                }
+                else
+                {
+                    MessageBox.Show("Could not create backup\n\nAborting.",
+                       "Error",
+                       MessageBoxButtons.OK,
+                       MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void ToggleContext()
+        {
+            _currentContext = _currentContext == Context.Global
+                ? Context.User
+                : Context.Global;
+
+            var title = _currentContext == Context.Global
+                ? "Switch to User"
+                : "Switch to Global";
+
+            menuItemSwitch.Text = title;
+
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            var variables = GetPathVariable(_currentContext);
+            InitListView(variables);
+        }
+
+        private void InitListView(List<string> vars)
+        {
+            listView1.Clear();
+
+            listView1.Scrollable = true;
+            listView1.CheckBoxes = true;
+            listView1.FullRowSelect = true;
+            listView1.View = View.Details;
+            listView1.Sorting = SortOrder.Ascending;
+
+            var header = new ColumnHeader();
+            header.Text = "Path (" + vars.Count.ToString() + ")";
+            header.Name = "col1";
+            header.Width = 600;
+            listView1.Columns.Add(header);
+
+            foreach (var v in vars)
+            {
+                var item = new ListViewItem(v);
+                item.Checked = true;
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void InvalidatePaths(Context ctx)
+        {
+            foreach(ListViewItem item in listView1.Items)
+            {
+                var exists = Directory.Exists(item.Text);
+
+                item.BackColor = exists ? Color.LightGreen : Color.Red;
+            }
+        }
+
+        private enum Context
+        {
+            Global = EnvironmentVariableTarget.Machine,
+            User = EnvironmentVariableTarget.User
+        }
+
+        private void LoadBackup(string path)
+        {
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File does not exists: " + path,
+                    "Error: File does not exists",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+
+                return;
+            }
+
+            var content = File.ReadAllText(path);
+
+            var vars = content.Split(';')
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToList<string>();
+
+            InitListView(vars);
+        }
+
+        private bool AutoBackup(Context ctx)
+        {
+            var pathVar = Environment.GetEnvironmentVariable("path",
+                (EnvironmentVariableTarget)ctx);
+
+            var name = string.Format("path-editor-auto-backup-{0}-{1}-{2}.txt",
+                    _currentContext.ToString(),
+                    DateTime.Now.ToShortDateString(),
+                    DateTime.Now.ToLongTimeString());
+
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), name);
+            
+            if (File.Exists(path))
+            {
+                MessageBox.Show("Could not create backup file. File already exists.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button2);
+
+                return false;
+            }
+
+            File.WriteAllText(path, pathVar);
+
+            return true;
+        }
+
+        private void CreateBackup(Context ctx, string filename)
+        {
+            var pathVar = Environment.GetEnvironmentVariable("path",
+                (EnvironmentVariableTarget)ctx);
+
+            if (File.Exists(filename))
+            {
+                MessageBox.Show("File already exists: " + filename, 
+                    "Error: File already exists", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Exclamation);
+
+                return;
+            }
+
+            File.WriteAllText(filename, pathVar);
+        }
+
+        private string GetBackupName()
+        {
+            return string.Format("path-editor-backup-{0}-{1}-{2}",
+                    _currentContext.ToString(),
+                    DateTime.Now.ToShortDateString(),
+                    DateTime.Now.ToLongTimeString());
+        }
+
+        private List<string> GetPathVariable(Context ctx)
+        {
+            var path = Environment.GetEnvironmentVariable("path",
+                (EnvironmentVariableTarget)ctx);
+
+            return path.Split(';')
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToList<string>();
+        }
+
+        private void SetPathVariable(Context ctx, string var)
+        {
+            var pathVar = ConstructPathFromList();
+
+            Environment.SetEnvironmentVariable("path", var,
+                (EnvironmentVariableTarget)ctx);
+        }
+
+        private string ConstructPathFromList()
+        {
+            List<string> vars = listView1.Items
+               .Cast<ListViewItem>()
+               .Where(x => x.Checked)
+               .Select(x => x.Text)
+               .ToList<string>();
+
+            return string.Join(";", vars);
+        }
+
+        private bool AddNewVar(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return false;
+
+            if (!Directory.Exists(text))
+            {
+                txtAdd.BackColor = Color.Red;
+                return false;
+            }
+            else
+                txtAdd.BackColor = DefaultBackColor;
+
+            var item = new ListViewItem();
+            item.Checked = true;
+            item.BackColor = Color.LightGreen;
+            item.Text = text
+                .TrimEnd()
+                .TrimStart();
+
+            listView1.Items.Add(item);
+
+            return true;
+        }
+    }
+}
